5289cb73ae61ec89945210f0d8efa8148e8b6599
x-jirox <correalenin045gmail.com>
2025-01-07 11:12:50 -0500

    Primer commit: Subida inicial del proyecto

---
+++ src/app/abilidades/abilidades.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abilidades',
+  templateUrl: './abilidades.component.html',
+  styleUrls: ['./abilidades.component.css']
+})
+export class AbilidadesComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { AbilidadesComponent } from './abilidades/abilidades.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SobreMiComponent,
+    AbilidadesComponent,
+    ProyectosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
